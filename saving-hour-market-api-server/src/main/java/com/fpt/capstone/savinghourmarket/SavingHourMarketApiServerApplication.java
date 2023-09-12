88b355a525eb07c6a3a84899374763b0b44bd15c
giavinh <82934507+luugiavinh@users.noreply.github.com>
2023-09-12 08:31:13 +0700

    create api server

---
+++ saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/SavingHourMarketApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.capstone.savinghourmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingHourMarketApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingHourMarketApiServerApplication.class, args);
+	}
+
+}
---
+++ saving-hour-market-api-server/src/test/java/com/fpt/capstone/savinghourmarket/SavingHourMarketApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.capstone.savinghourmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingHourMarketApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
