5da394bb3ea8906d9eff470d26eaf609b5ece05b
hieu <nguyentrunghieu13012002@gmail.com>
2023-09-20 15:46:07 +0700

    Update Order.java

--- saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/entity/Order.java
+++ saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/entity/Order.java
@@ -37,6 +37,9 @@ public class Order {
     @Column(columnDefinition = "tinyint")
     private Integer status;
 
+    @Column(columnDefinition = "tinyint")
+    private Integer payment_method;
+
     @Column(columnDefinition = "varchar(255)")
     private String addressDeliver;
 
