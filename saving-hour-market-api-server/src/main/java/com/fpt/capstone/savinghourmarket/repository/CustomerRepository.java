6ccb442a8e0d54369b31a1a4fc5850696b20fc40
giavinh <82934507+luugiavinh@users.noreply.github.com>
2023-10-04 17:13:18 +0700

    create schedule to delete unverified email

--- saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/repository/CustomerRepository.java
+++ saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/repository/CustomerRepository.java
@@ -2,8 +2,11 @@ package com.fpt.capstone.savinghourmarket.repository;
 
 import com.fpt.capstone.savinghourmarket.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +16,8 @@ public interface CustomerRepository extends JpaRepository<Customer, UUID> {
     Optional<Customer> getCustomerByEmail (String email);
 
     Optional<Customer> findByPhoneAndFullName(String phone, String fullName);
+
+    @Modifying
+    @Query("delete from Customer c where c.email in ?1")
+    void deleteCustomersWithIds(List<String> ids);
 }
