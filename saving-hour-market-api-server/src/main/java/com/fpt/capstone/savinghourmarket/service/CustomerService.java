ae813c860b97d4b73914a6e76ce1920b7c214a65
giavinh <82934507+luugiavinh@users.noreply.github.com>
2023-10-04 11:22:42 +0700

    fix updateInfo customer receive RequestBody + fix get all cate and sub cate

--- saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/service/CustomerService.java
+++ saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/service/CustomerService.java
@@ -5,7 +5,6 @@ import com.fpt.capstone.savinghourmarket.model.PasswordRequestBody;
 import com.fpt.capstone.savinghourmarket.model.CustomerRegisterRequestBody;
 import com.fpt.capstone.savinghourmarket.model.CustomerUpdateRequestBody;
 import com.google.firebase.auth.FirebaseAuthException;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -17,7 +16,7 @@ public interface CustomerService {
 
     Customer getInfo(String email);
 
-    Customer updateInfo(CustomerUpdateRequestBody customerUpdateRequestBody, String email, MultipartFile imageFile) throws IOException;
+    Customer updateInfo(CustomerUpdateRequestBody customerUpdateRequestBody, String email) throws IOException;
 
     void updatePassword(PasswordRequestBody passwordRequestBody, String email) throws FirebaseAuthException;
 }
