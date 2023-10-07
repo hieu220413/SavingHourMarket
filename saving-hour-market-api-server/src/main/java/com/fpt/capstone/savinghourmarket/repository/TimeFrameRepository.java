b02245efc67e85ddde0d7051373ffbe8cc2eeee9
giavinh <82934507+luugiavinh@users.noreply.github.com>
2023-10-07 14:42:18 +0700

    change timeframe function

--- saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/repository/TimeFrameRepository.java
+++ saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/repository/TimeFrameRepository.java
@@ -2,10 +2,19 @@ package com.fpt.capstone.savinghourmarket.repository;
 
 import com.fpt.capstone.savinghourmarket.entity.TimeFrame;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TimeFrameRepository extends JpaRepository<TimeFrame, UUID> {
+    @Query("SELECT DISTINCT og.timeFrame FROM OrderGroup og ")
+    List<TimeFrame> findForPickupPoint();
+
+    @Query("SELECT tf FROM TimeFrame tf " +
+            "WHERE tf.toHour <= :endTime")
+    List<TimeFrame> findForHomeDelivery(LocalTime endTime);
 }
