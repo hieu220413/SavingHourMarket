97062f97dc36976629f1cf6f0ed9022bf8f5eb45
giavinh <82934507+luugiavinh@users.noreply.github.com>
2023-10-20 22:10:40 +0700

    Update RevenueReportMonthly.java

--- saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/model/RevenueReportMonthly.java
+++ saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/model/RevenueReportMonthly.java
@@ -11,17 +11,25 @@ import lombok.Setter;
 public class RevenueReportMonthly {
     public RevenueReportMonthly(Integer monthValue, RevenueReportResponseBody revenue) {
         this.monthValue = monthValue;
-        this.revenue = revenue;
+//        this.revenue = revenue;
         this.month = Month.getMonthNameFromNumber(monthValue);
+        this.totalIncome = revenue.getTotalIncome();
+        this.totalPriceOriginal = revenue.getTotalPriceOriginal();
+        this.totalDifferentAmount = revenue.getTotalDifferentAmount();
+        this.totalSale = revenue.getTotalSale();
     }
 
-    public RevenueReportMonthly(Integer monthValue) {
-        this.monthValue = monthValue;
-        this.month = Month.getMonthNameFromNumber(monthValue);
-        this.revenue = new RevenueReportResponseBody();
-    }
+//    public RevenueReportMonthly(Integer monthValue) {
+//        this.monthValue = monthValue;
+//        this.month = Month.getMonthNameFromNumber(monthValue);
+////        this.revenue = new RevenueReportResponseBody();
+//    }
 
     private Integer monthValue;
     private String month;
-    private RevenueReportResponseBody revenue;
+//    private RevenueReportResponseBody revenue;
+    private Long totalIncome;
+    private Long totalPriceOriginal;
+    private Long totalDifferentAmount;
+    private Long totalSale;
 }
