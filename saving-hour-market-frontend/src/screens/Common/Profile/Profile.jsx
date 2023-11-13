878b1cce4d9ca0348a2ea7ffc4680f35d2478824
Phạm Nguyễn Hùng Anh <hunganh130502@gmail.com>
2023-11-14 04:07:40 +0700

    Selection Staff

--- saving-hour-market-frontend/src/screens/Common/Profile/Profile.jsx
+++ saving-hour-market-frontend/src/screens/Common/Profile/Profile.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Profile.scss";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const user = useSelector((state) => state.user.user);
   return (
     <div className="profile__wrapper">
       <div className="profile__container">
@@ -11,9 +13,9 @@ const Profile = () => {
             alt=""
           />
         </div>
-        <h2 className="profile__name">Hà Anh Tú</h2>
-        <h2 className="profile__email">tuhase161714@fpt.edu.vn</h2>
-        <h2 className="profile__email">Product selection</h2>
+        <h2 className="profile__name">{user.fullName}</h2>
+        <h2 className="profile__email">{user.email}</h2>
+        <h2 className="profile__email">{user.role}</h2>
         <div className="profile__changepassword">Đổi mật khẩu</div>
       </div>
     </div>
