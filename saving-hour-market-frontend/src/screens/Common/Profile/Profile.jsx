cb045878ad19c8d7dcfd885686e13d8e3d57a0bc
Phạm Nguyễn Hùng Anh <hunganh130502@gmail.com>
2023-11-14 04:10:19 +0700

    fix xiu xiu

--- saving-hour-market-frontend/src/screens/Common/Profile/Profile.jsx
+++ saving-hour-market-frontend/src/screens/Common/Profile/Profile.jsx
@@ -4,14 +4,12 @@ import { useSelector } from "react-redux";
 
 const Profile = () => {
   const user = useSelector((state) => state.user.user);
+  console.log("user", user);
   return (
     <div className="profile__wrapper">
       <div className="profile__container">
         <div className="profile__img">
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/capstone-project-398104.appspot.com/o/public%2Fdefault-avatar.jpg?alt=media"
-            alt=""
-          />
+          <img src={user.avatarUrl} alt="" />
         </div>
         <h2 className="profile__name">{user.fullName}</h2>
         <h2 className="profile__email">{user.email}</h2>
