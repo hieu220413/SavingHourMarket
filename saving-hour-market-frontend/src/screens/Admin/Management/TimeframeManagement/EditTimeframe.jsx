bf599c3a303c4013ab605babc598464bd0f64c72
haanhtu0502 <haanhtu0502@gmail.com>
2023-11-14 14:28:41 +0700

    time frame + consolidation

--- saving-hour-market-frontend/src/screens/Admin/Management/TimeframeManagement/EditTimeframe.jsx
+++ saving-hour-market-frontend/src/screens/Admin/Management/TimeframeManagement/EditTimeframe.jsx
@@ -10,23 +10,146 @@ import React, { useState } from "react";
 import "./TimeframeManagement.scss";
 import { format } from "date-fns";
 import dayjs from "dayjs";
+import MuiAlert from "@mui/material/Alert";
+import { Dialog, Snackbar } from "@mui/material";
+import { API } from "../../../../contanst/api";
+import { auth } from "../../../../firebase/firebase.config";
+import LoadingScreen from "../../../../components/LoadingScreen/LoadingScreen";
 
-const EditTimeframe = ({ handleClose }) => {
-  const [selectedDayOfWeek, setSelectedDayOfWeek] = useState(null);
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const EditTimeframe = ({
+  handleClose,
+  setOpenSnackbar,
+  openSnackbar,
+  setTimeframeList,
+  searchValue,
+  page,
+  setTotalPage,
+  timeframeItem,
+}) => {
+  const deliveryMethods = [
+    { display: "Điểm giao hàng", value: "PICKUP_POINT" },
+    { display: "Giao tận nhà", value: "DOOR_TO_DOOR" },
+    { display: "Tất cả", value: "ALL" },
+  ];
+  const [selectedDeliveryMethod, setSelectedDeliveryMethod] = useState(
+    deliveryMethods.find(
+      (item, index) => index === timeframeItem.allowableDeliverMethod
+    )
+  );
   const [openDayOfWeekDropdown, setOpenDayOfWeekDropdown] = useState(false);
   const [fromHour, setFromHour] = useState(null);
-  const [toHour, setToHour] = useState(new Date("10:15"));
-  const daysOfWeek = [
-    { display: "Thứ hai", value: 1 },
-    { display: "Thứ ba", value: 1 },
-    { display: "Thứ tư", value: 1 },
-    { display: "Thứ năm", value: 1 },
-    { display: "Thứ sáu", value: 1 },
-    { display: "Thứ bảy", value: 1 },
-    { display: "Chủ nhật", value: 1 },
-    { display: "Mỗi ngày", value: 1 },
-    { display: "Cuối tuần", value: 1 },
-  ];
+  const [toHour, setToHour] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    deliveryMethods: "",
+    fromHour: "",
+    toHour: "",
+  });
+  const [openValidateSnackbar, setOpenValidateSnackbar] = useState({
+    open: false,
+    vertical: "top",
+    horizontal: "right",
+    severity: "error",
+    text: "",
+  });
+  const { vertical, horizontal } = openValidateSnackbar;
+  const handleCloseValidateSnackbar = () => {
+    setOpenValidateSnackbar({ ...openValidateSnackbar, open: false });
+  };
+
+  const handleCreate = async () => {
+    if (!selectedDeliveryMethod) {
+      setError({ ...error, deliveryMethods: "Vui lòng không để trống" });
+      return;
+    }
+
+    if (fromHour && toHour && fromHour >= toHour) {
+      setOpenValidateSnackbar({
+        ...openValidateSnackbar,
+        open: true,
+        text: "Giờ kết thúc không thể sớm hơn giờ bắt đầu",
+      });
+      return;
+    }
+    if (fromHour && !toHour && fromHour >= timeframeItem.toHour) {
+      setOpenValidateSnackbar({
+        ...openValidateSnackbar,
+        open: true,
+        text: "Giờ kết thúc không thể sớm hơn giờ bắt đầu",
+      });
+      return;
+    }
+    if (!fromHour && toHour && timeframeItem.fromHour >= toHour) {
+      setOpenValidateSnackbar({
+        ...openValidateSnackbar,
+        open: true,
+        text: "Giờ kết thúc không thể sớm hơn giờ bắt đầu",
+      });
+      return;
+    }
+    let submitCreate = {
+      fromHour: fromHour ? fromHour : timeframeItem.fromHour,
+      toHour: toHour ? toHour : timeframeItem.toHour,
+      allowableDeliverMethod: selectedDeliveryMethod.value,
+    };
+
+    console.log(submitCreate);
+
+    setLoading(true);
+    const tokenId = await auth.currentUser.getIdToken();
+    fetch(
+      `${API.baseURL}/api/timeframe/update?timeFrameId=${timeframeItem.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${tokenId}`,
+        },
+        body: JSON.stringify(submitCreate),
+      }
+    )
+      .then((res) => res.json())
+      .then((respond) => {
+        console.log(respond);
+
+        fetch(
+          `${API.baseURL}/api/timeframe/getAllForAdmin?page=${
+            page - 1
+          }&limit=6&enableDisableStatus=ENABLE
+          `,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${tokenId}`,
+            },
+          }
+        )
+          .then((res) => res.json())
+          .then((respond) => {
+            if (respond?.code === 404 || respond.status === 500) {
+              setLoading(false);
+              return;
+            }
+            setTimeframeList(respond.pickupPointList);
+            setTotalPage(respond.totalPage);
+            handleClose();
+            setOpenSnackbar({
+              ...openSnackbar,
+              open: true,
+              severity: "success",
+              text: "Chỉnh sửa thành công !",
+            });
+            setLoading(false);
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => {});
+  };
   return (
     <div style={{ width: 450 }} className={`modal__container `}>
       {/* // modal header */}
@@ -38,7 +161,7 @@ const EditTimeframe = ({ handleClose }) => {
       <div className={`modal__container-body `}>
         <div className="modal__container-body-inputcontrol">
           <h4 className="modal__container-body-inputcontrol-label">
-            Ngày trong tuần
+            Phương thức giao hàng
           </h4>
           <div>
             <div
@@ -56,21 +179,19 @@ const EditTimeframe = ({ handleClose }) => {
                     setOpenDayOfWeekDropdown(!openDayOfWeekDropdown)
                   }
                 >
-                  {selectedDayOfWeek
-                    ? selectedDayOfWeek.display
-                    : "Chọn ngày trong tuần"}
+                  {selectedDeliveryMethod
+                    ? selectedDeliveryMethod.display
+                    : "Phương thức giao hàng"}
                   <FontAwesomeIcon icon={faCaretDown} />
                 </div>
                 {openDayOfWeekDropdown && (
-                  <div
-                    style={{ height: "180px", overflowY: "scroll" }}
-                    className="dropdown-content"
-                  >
-                    {daysOfWeek.map((item, index) => (
+                  <div style={{ width: 169 }} className="dropdown-content">
+                    {deliveryMethods.map((item, index) => (
                       <div
                         onClick={(e) => {
-                          setSelectedDayOfWeek(item);
+                          setSelectedDeliveryMethod(item);
                           setOpenDayOfWeekDropdown(false);
+                          setError({ ...error, deliveryMethods: "" });
                         }}
                         className="dropdown-item"
                         key={index}
@@ -82,14 +203,14 @@ const EditTimeframe = ({ handleClose }) => {
                 )}
               </div>
             </div>
-            {/* {error.supermarket && (
-            <p
-              style={{ fontSize: "14px", marginBottom: "-10px" }}
-              className="text-danger"
-            >
-              {error.supermarket}
-            </p>
-          )} */}
+            {error.deliveryMethods && (
+              <p
+                style={{ fontSize: "14px", marginBottom: "-10px" }}
+                className="text-danger"
+              >
+                {error.deliveryMethods}
+              </p>
+            )}
           </div>
         </div>
 
@@ -100,19 +221,25 @@ const EditTimeframe = ({ handleClose }) => {
           <div className="timepicker-control">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <TimePicker
+                defaultValue={dayjs(
+                  format(dayjs(new Date()).$d, "yyyy-MM-dd").concat(
+                    timeframeItem.fromHour
+                  )
+                )}
                 onChange={(e) => {
                   setFromHour(format(e.$d, "HH:mm:ss"));
+                  setError({ ...error, fromHour: "" });
                 }}
               />
             </LocalizationProvider>
-            {/* {error.name && (
-          <p
-            style={{ fontSize: "14px", marginBottom: "-10px" }}
-            className="text-danger"
-          >
-            {error.name}
-          </p>
-        )} */}
+            {error.fromHour && (
+              <p
+                style={{ fontSize: "14px", marginBottom: "-10px" }}
+                className="text-danger"
+              >
+                {error.fromHour}
+              </p>
+            )}
           </div>
         </div>
         <div className="modal__container-body-inputcontrol">
@@ -122,22 +249,25 @@ const EditTimeframe = ({ handleClose }) => {
           <div className="timepicker-control">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <TimePicker
-                // defaultValue={dayjs(
-                //   format(dayjs(new Date()).$d, "yyyy-MM-dd").concat("10:15:00")
-                // )}
+                defaultValue={dayjs(
+                  format(dayjs(new Date()).$d, "yyyy-MM-dd").concat(
+                    timeframeItem.toHour
+                  )
+                )}
                 onChange={(e) => {
                   setToHour(format(e.$d, "HH:mm:ss"));
+                  setError({ ...error, toHour: "" });
                 }}
               />
             </LocalizationProvider>
-            {/* {error.name && (
-          <p
-            style={{ fontSize: "14px", marginBottom: "-10px" }}
-            className="text-danger"
-          >
-            {error.name}
-          </p>
-        )} */}
+            {error.toHour && (
+              <p
+                style={{ fontSize: "14px", marginBottom: "-10px" }}
+                className="text-danger"
+              >
+                {error.toHour}
+              </p>
+            )}
           </div>
         </div>
       </div>
@@ -150,12 +280,34 @@ const EditTimeframe = ({ handleClose }) => {
           >
             Đóng
           </button>
-          <button className="modal__container-footer-buttons-create">
-            Chỉnh sửa
+          <button
+            onClick={handleCreate}
+            className="modal__container-footer-buttons-create"
+          >
+            Tạo mới
           </button>
         </div>
       </div>
       {/* *********************** */}
+      <Snackbar
+        open={openValidateSnackbar.open}
+        autoHideDuration={1500}
+        anchorOrigin={{ vertical, horizontal }}
+        onClose={handleCloseValidateSnackbar}
+      >
+        <Alert
+          onClose={handleCloseValidateSnackbar}
+          severity={openValidateSnackbar.severity}
+          sx={{
+            width: "100%",
+            fontSize: "15px",
+            alignItem: "center",
+          }}
+        >
+          {openValidateSnackbar.text}
+        </Alert>
+      </Snackbar>
+      {loading && <LoadingScreen />}
     </div>
   );
 };
