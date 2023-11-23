d28b48d43594ee287d90ee585311a51801026b02
hieu <nguyentrunghieu13012002@gmail.com>
2023-11-23 16:48:42 +0700

    update confirm Packaging and Packaged Group

--- saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/repository/OrderBatchRepository.java
+++ saving-hour-market-api-server/src/main/java/com/fpt/capstone/savinghourmarket/repository/OrderBatchRepository.java
@@ -30,10 +30,8 @@ public interface OrderBatchRepository extends JpaRepository<OrderBatch, UUID> {
             "AND " +
             "((:delivererId IS NULL) OR (ob.deliverer.id = :delivererId)) " +
             "AND " +
-            "((:status IS NULL) OR (EXISTS (SELECT 1 FROM Order o WHERE o IN elements(ob.orderList) AND o.status = :status))) " +
-            "AND " +
             "(ob.orderList IS NOT EMPTY) ")
-    List<OrderBatch> findByDistrictOrDeliverDate(Integer status,Boolean getOldOrderBatch, LocalDate deliveryDate, UUID delivererId, Sort sort);
+    List<OrderBatch> findByDistrictOrDeliverDate(Boolean getOldOrderBatch, LocalDate deliveryDate, UUID delivererId, Sort sort);
 
     @Query("SELECT ob FROM OrderBatch ob " +
             "JOIN ob.deliverer delv " +
